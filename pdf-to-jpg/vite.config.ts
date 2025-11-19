22f06fce14c738083177d2931fc48c3f1ba0a565
fmorondo <fmorondo@gmail.com>
2025-11-19 20:18:11 +0000

    fix: Corrige ruta base de Vite y despliega aplicacion PDF a JPG

--- pdf-to-jpg/vite.config.ts
+++ pdf-to-jpg/vite.config.ts
@@ -1,10 +1,11 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // FIX: Configura la base para el despliegue en subcarpeta de GitHub Pages
+  base: '/pdf-to-jpg/',
   server: {
     port: 3000, // Puerto para desarrollo local
   }
